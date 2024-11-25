a9e3bc0ee109aef9171efe88d9abea87809d73dd
FahadEbrahim <62794766+FahadEbrahim@users.noreply.github.com>
2024-11-25 16:04:10 +0000

    Update run_all_full.sh

--- scripts/run_all_full.sh
+++ scripts/run_all_full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# List of names
+# List of model names
 models=("codebert" "graphcodebert" "unixcoder" "codeberta" "codet5" "plbart")
 
 # Path to the Python script
@@ -9,11 +9,11 @@ script_path="run_scpd.py"
 # List of datasets
 datasets=(1 2)
 
-# Loop through the datasets and names, and execute the Python script
+# Loop through the datasets and model names, and execute the Python script
 for dataset in "${datasets[@]}"; do
     for model in "${models[@]}"; do
         echo "Running full fine tuning script for the dataset: $dataset and model: $model"
-        python "$script_path" --dataset_version "$dataset" --learning_rate 1e-5 --tune_type fft --epoch 50 --report wandb --model_name "$model"
+        python "$script_path" --dataset_version "$dataset" --learning_rate 1e-5 --tune_type fft --epoch 30 --report wandb --model_name "$model" --use_callback --pre_process --ft_metric f_beta_score
         echo "-------------------------------------"
     done
 done
