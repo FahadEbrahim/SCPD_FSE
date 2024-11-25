2cbae9c41f886a1a157d926a8ca3bdf23565ffc4
FahadEbrahim <62794766+FahadEbrahim@users.noreply.github.com>
2024-11-25 15:57:07 +0000

    Update run_all_adapters.sh

--- scripts/run_all_adapters.sh
+++ scripts/run_all_adapters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of adapter and model names
-adapters=("houlsby" "pfeiffer" "lora" "ia3" "parallel" "prefixtuning")
+adapters=("houlsby" "pfeiffer" "lora")
 models=("codebert" "graphcodebert" "unixcoder" "codeberta" "codet5" "plbart")
 
 # Path to the Python script
@@ -15,7 +15,7 @@ for dataset in "${datasets[@]}"; do
     for adapter in "${adapters[@]}"; do
         for model in "${models[@]}"; do
             echo "Running script for the dataset: $dataset, adapter: $adapter, and model: $model"
-            python "$script_path" --dataset_version "$dataset" --model_name "$model" --tune_type peft --epoch 50 --report wandb --adapter_type "$adapter"
+            python "$script_path" --dataset_version "$dataset" --model_name "$model" --tune_type peft --epoch 30 --report wandb --adapter_type "$adapter" --pre_processing --ft_metric f_beta_score --use_callback --class_weights
             echo "-------------------------------------"
         done
     done
